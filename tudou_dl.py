45a042179485368656256da81377bd3004ecbb94
yi.codeplayer <yi.codeplayer@0d5d7f64-d523-0410-bc7f-a9ab43a475e4>
2007-01-09 10:51:43 +0000

    获取 tudou 视频真实的下载地址...
    git-svn-id: https://huangyilib.googlecode.com/svn/trunk@3 0d5d7f64-d523-0410-bc7f-a9ab43a475e4

---
+++ tudou_dl.py
@@ -0,0 +1,39 @@
+import sys
+import re
+import urllib2
+
+info_url = 'http://www.tudou.com/player/i.php'
+standard_url_tmp = 'http://www.tudou.com/programs/view/%s/'
+querystring_tmp = 'id=%s&retry=%s'
+iid_re = re.compile('var iid=(\d+)', re.M)
+input_url_re = re.compile(r'(?:http://)?(?:www\d*\.)?tudou\.com/programs/view/(\w+)/?')
+
+# get the input url
+if len(sys.argv)<2 or sys.argv[1] in ['-h', '--help']:
+    print '''usage: tudou_dl.py url'''
+    sys.exit(1)
+url = sys.argv[1]
+
+# Verify the input url and build a standard one
+match = input_url_re.match(url)
+if not match:
+    print 'please input a correct url.'
+    sys.exit(1)
+url = standard_url_tmp % match.group(1)
+
+print 'fetching the page', url, '...'
+page = urllib2.urlopen(url).read()
+
+# get the iid
+print 'analoging current page...'
+match = iid_re.search(page)
+if not match:
+    print 'can\'t find the iid'
+    sys.exit(1)
+iid = match.group(1)
+print 'the iid', iid, 'is found'
+
+# construct the querystring, and post to info_url
+querystring = querystring_tmp % (iid, 0)
+real_url = urllib2.urlopen(info_url, querystring).read()
+print 'the real video url is', real_url
