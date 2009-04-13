8fc5b40a9fe7e5a352c830a385ebd712cedb0b05
yi.codeplayer <yi.codeplayer@0d5d7f64-d523-0410-bc7f-a9ab43a475e4>
2009-04-13 12:03:43 +0000

    更多文档和注释
    git-svn-id: https://huangyilib.googlecode.com/svn/trunk@39 0d5d7f64-d523-0410-bc7f-a9ab43a475e4

--- 面试题/solution.py
+++ 面试题/solution.py
@@ -1,22 +1,27 @@
 #!/usr/bin/env python
 # coding: utf-8
 '''
-设计
-=====
+===========================
+Tower Defense (Stage I) 
+===========================
 
-先把待建塔楼建上去，然后深度优先遍历地图，看能否联通。
+思路：先把待建塔楼建上去，然后深度优先遍历地图，看能否联通。
+
+开发环境：Python2.5
+
+设计要点
+=========
+
+使用二维列表表示地图。
+
+使用元组 tuple 表示节点坐标。
+
+使用深度优先算法遍历地图。
 
 坐标系统
 =========
 
 左上角为原点，垂直方向为 X 轴，水平方向为 Y 轴（该坐标系统由问题描述中所定义）。
-
-单词缩写
-=========
-
-* pos => position
-* infile => input file
-* outfile => output file
 
 输入文件格式
 =============
@@ -36,11 +41,24 @@
 使用方法
 =========
 
-程序使用标准输入读取输入数据，标准输出输出信息。
+::
 
-可以直接执行程序交互式输入样本输入（注意要输入 Ctrl+Z 结束输入）， ::
+    $ solution.py --help
+    Usage: solution.py [-f] [-o] [-t]
 
-    $ python solution.py
+    Options:
+      --version             show program's version number and exit
+      -h, --help            show this help message and exit
+      -f FILE, --file=FILE  input file, default to stdin
+      -o OUTPUT, --output=OUTPUT
+                            output file, default to stdout
+      -t, --test            run doc test
+
+程序可以使用命令行参数指定输入输出文件，默认使用标准输入输出。
+
+可以直接执行程序交互式输入样本数据（注意要输入 Ctrl+Z 结束输入）， ::
+
+    $ ./solution.py
     3 4
     - - - -
     - * - -
@@ -49,24 +67,24 @@
     ^Z
     0 1 No
 
-也可先将问题描述写入文件，通过输入重定向执行程序。 ::
+也可先将问题描述写入文件，指定输入文件名来执行。 ::
 
-    $ python solution.py < input_file
+    $ ./solution.py -f input_file
     0 1 No
 
 执行测试
 =========
 
-* 传递 doctest 作为参数执行文档测试。
-* 执行 test_solution.py 执行单元测试。
+* 传递 -t 或 --test 参数执行文档测试。
+* ./test_solution.py 执行单元测试。
 '''
 
 def get_input(infile):
     '''
-    解析输入文件，构建地图，并将待建塔楼先建上去
+    解析输入文件，构建地图，并将待建塔楼建上去
 
     参数：输入文件对象
-    输出：地图，需要抵达的目标位置，待建塔楼坐标
+    输出：地图，需要抵达的目标节点坐标，待建塔楼坐标
 
     示例：
 
@@ -86,23 +104,25 @@ def get_input(infile):
     '''
     it = iter(infile)
     line = it.next()
+    # 地图规格
     M, N = map(int, line.split())
+    # 构建地图
     graph = [[0 for j in range(N)] for i in range(M)]
     for i in range(M):
         flags = it.next().split()
         for j in range(N):
             if flags[j]=='*':
                 graph[i][j] = 1
-    tower_x, tower_y = map(int, it.next().split())
     # 建立目标塔楼
+    tower_x, tower_y = map(int, it.next().split())
     graph[tower_x][tower_y] = 1
     return graph, (M-1, N-1), (tower_x, tower_y)
 
-def get_next_poses(pos):
+def get_next_positions(position):
     '''
-    获取相邻节点位置
+    获取相邻节点坐标
     '''
-    x, y = pos
+    x, y = position
     # 优先右下角方向
     return [(x-1, y), (x, y-1), (x+1,y), (x, y+1)]
 
@@ -110,8 +130,8 @@ def traverse(graph, size, start, target):
     '''
     使用循环对地图进行深度优先遍历
 
-    输入：地图，地图大小，需抵达的目标位置
-    返回：bool值，表示从起始位置到目标位置能否联通
+    输入：地图，地图大小，需抵达的目标节点坐标
+    返回：bool值，表示从起始节点到目标节点能否联通
 
     示例：
     >>> traverse([[0, 0, 0, 0], [0, 1, 0, 0], [0, 1, 0, 0]], (3, 4), (0, 0), (2, 3))
@@ -121,26 +141,27 @@ def traverse(graph, size, start, target):
     '''
     # 待访问节点栈
     visit_queue = [start]
-    # 记录已访问节点位置
+    # 记录已访问节点坐标
     visited = {}
 
-    def test_pos(pos):
+    def test_position(position):
         '''
-        测试该位置是否要加入待访问节点队列
+        测试该节点是否要加入待访问节点队列
         '''
-        x, y = pos
+        x, y = position
         M,N = size
-        return pos not in visited and M>x>=0 and N>y>=0 and graph[x][y]==0
+        return position not in visited and M>x>=0 and N>y>=0 and graph[x][y]==0
 
     while visit_queue:
         #print 'visit_queue', visit_queue
-        pos = visit_queue.pop()
-        visited[pos] = 1
-        #print 'visited', pos
-        if pos==target:
+        position = visit_queue.pop()
+        visited[position] = 1
+        #print 'visited', position
+        if position==target:
             return True
-        next_poses = filter(test_pos, get_next_poses(pos))
-        visit_queue += next_poses
+        # 产生相邻节点并加入待访问节点队列中。
+        next_positions = filter(test_position, get_next_positions(position))
+        visit_queue += next_positions
     return False
 
 def main(infile, outfile):
@@ -151,14 +172,33 @@ def main(infile, outfile):
     graph, target_position, tower_position = get_input(infile)
     #print graph
     # 深度优先遍历地图
-    result = traverse(graph, (len(graph), len(graph[0])), (0, 0), target_position)
+    connected = traverse(graph, (len(graph), len(graph[0])), (0, 0), target_position)
     # 输出结果
-    outfile.write('%d %d %s'%(tower_position[0], tower_position[1], result and 'Yes' or 'No'))
+    outfile.write('%d %d %s'%(tower_position[0], tower_position[1], connected and 'Yes' or 'No'))
 
 if __name__ == '__main__':
-    import sys
-    if len(sys.argv)>1 and sys.argv[1]=='doctest':
+    import sys, optparse
+    parser = optparse.OptionParser(usage='%prog [-f] [-o] [-t]', version='%prog 1.0')
+    parser.add_option("-f", "--file",
+            help="input file, default to stdin", default=sys.stdin)
+    parser.add_option("-o", "--output",
+            help="output file, default to stdout", default=sys.stdout)
+    parser.add_option("-t", "--test", action="store_true", dest="test",
+            help="run doc test")
+    (options, args) = parser.parse_args()
+
+    if options.test:
+        # 执行文档测试
         import doctest
-        doctest.testmod()
+        doctest.testmod(verbose=True)
     else: 
-        main(sys.stdin, sys.stdout)
+        infile = options.file
+        outfile = options.output
+        try:
+            if isinstance(options.file, str):
+                infile = open(infile)
+            if isinstance(options.output, str):
+                outfile = open(outfile, 'w')
+        except IOError, ex:
+            print ex
+        main(infile, outfile)
