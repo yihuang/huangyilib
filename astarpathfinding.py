7e93cad837de4e572d2650184fc640ff411574c0
yi.codeplayer <yi.codeplayer@0d5d7f64-d523-0410-bc7f-a9ab43a475e4>
2010-03-06 05:57:31 +0000

    git-svn-id: https://huangyilib.googlecode.com/svn/trunk@89 0d5d7f64-d523-0410-bc7f-a9ab43a475e4

--- astarpathfinding.py
+++ astarpathfinding.py
@@ -13,12 +13,26 @@ def vecadd((x1,y1), (x2,y2)):
 
 class Node(object):
     def __init__(self, pos, parent, dsrc, dtarget):
-        self.pos,self.parent,self.dsrc,self.dtarget = \
-                pos,parent,dsrc,dtarget
-        self.hvalue = dsrc+dtarget
+        self.pos,self.parent,self.dtarget,self.dsrc = \
+                pos,parent,dtarget,dsrc
+
+    def set_dsrc(self, value):
+        self._dsrc = value
+        self.hvalue = value+self.dtarget
+
+    def get_dsrc(self):
+        return self._dsrc
+
+    dsrc = property(get_dsrc, set_dsrc)
 
     def __cmp__(self, node):
         return self.hvalue - node.hvalue
+
+def findnode(l, pos):
+    for node in l:
+        if node.pos==pos:
+            return node
+    return None
 
 class Map(object):
     def __init__(self, desc):
@@ -33,12 +47,14 @@ class Map(object):
                     self.start = (x, y)
                 elif c=='3':
                     self.target = (x, y)
+        self.dtarget = {}
+        self.calc_all_dtarget()
         self.debug_map = deepcopy(self.text_map)
 
     def print_lines(self, map_):
         print '\n'.join(''.join(line) for line in map_)
 
-    def calc_htarget(self, (x,y)):
+    def calc_dtarget(self, (x,y)):
         tx,ty = self.target
         '''
         return abs(x-tx) + abs(y-ty)
@@ -46,6 +62,10 @@ class Map(object):
         dx,dy = abs(x-tx), abs(y-ty)
         dmax,dmin = dx>dy and (dx,dy) or (dy,dx)
         return dmax*10 + dmin*4
+    def calc_all_dtarget(self):
+        for pos, value in self.map.items():
+            if value==0:
+                self.dtarget[pos] = self.calc_dtarget(pos)
 
     def children(self, node):
         '''
@@ -66,29 +86,22 @@ class Map(object):
             (-1,1),
             (-1,0)
         ]
-        for d in directions:
-            childpos = vecadd(node.pos, d)
+        (x,y) = node.pos
+        for dx,dy in directions:
+            childpos = (x+dx, y+dy)
             if self.map[childpos]==0:
-                htarget = self.calc_htarget(childpos)
-                yield Node(childpos,node,node.dsrc+1,htarget)
+                yield childpos,node.dsrc+1
 
     def findpath(self):
-        path = []
+        startnode = Node(self.start, None, 0, 0)
         target = self.target
-        tovisit = [Node(self.start, None, 0, 0)]
-        visited = set() # record visited positions
-
-        while tovisit:
-            node = heappop(tovisit)
-            pos = node.pos
-
-            if pos==target:
+        openset = [startnode]   # node list
+        closeset = set()        # position set
+        while openset:
+            node = heappop(openset)
+            if node.pos == target:
                 return node
-
-            if pos in visited:
-                continue
-
-            visited.add(pos)
+            closeset.add(node.pos)
 
             #debug
             '''
@@ -98,27 +111,68 @@ class Map(object):
             pdb.set_trace()
             '''
 
-            for child in self.children(node):
-                if child.pos not in visited:
-                    heappush(tovisit, child)
+            for childpos, dsrc in self.children(node):
+                if childpos in closeset:
+                    continue
+                opennode = findnode(openset, childpos)
+                if opennode and dsrc>=opennode.dsrc:
+                    continue
+                elif not opennode:
+                    child = Node(childpos, node, dsrc, self.dtarget[childpos])
+                    heappush(openset, child)
+                else:
+                    opennode.dsrc = dsrc
+                    opennode.parent = node
+                    heapify(openset)
+
+    def findpath1(self):
+        startnode = Node(self.start, None, 0, 0)
+        target = self.target
+        openset = [startnode]   # node list
+        closeset = set()        # position set
+        while openset:
+            node = heappop(openset)
+            pos = node.pos
+            if pos in closeset:
+                continue
+            if pos == target:
+                return node
+            closeset.add(pos)
+
+            #debug
+            '''
+            x, y = pos
+            self.debug_map[y][x] = '*'
+            self.print_lines(self.debug_map)
+            pdb.set_trace()
+            '''
+
+            for childpos, dsrc in self.children(node):
+                if childpos in closeset:
+                    continue
+                child = Node(childpos, node, dsrc, self.dtarget[childpos])
+                heappush(openset, child)
 
 if __name__ == '__main__':
     graph_desc = '''
 0000000000000000000100000
-0000000000000000000103010
-0000000000000000000111110
-0000000000000000000100000
-0000000001111110000100000
-0000000001000110111100000
-0000000001012100010000000
-0000000001011100010000000
-0000000001000001010000000
-0000000001111111010000000
-0000000000000000010000000
+0111111111111111110103010
+0100000000000000000111110
+0101111111111111111100010
+0101000000000000000100010
+0101000001000110111100010
+0101000001012100010000010
+0101000001011100010000010
+0101000001000001010000010
+0100000001111111010000010
+0111111111111111111111110
 0000000000000000000000000
     '''
     map = Map(graph_desc)
-    node = map.findpath()
+
+    for i in range(1000):
+        node = map.findpath()
+
     while node:
         x,y = node.pos
         map.text_map[y][x] = '*'
